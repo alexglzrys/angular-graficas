6a59c928774594a48b03477ca54e176b317d2685
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-21 13:33:35 -0600

    gráfica de barras

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -7,6 +7,7 @@ import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.componen
 import { DonaComponent } from './pages/dona/dona.component';
 import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
@@ -19,7 +20,15 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    /**
+     * Cuando se trabaja con librerías que estan basadas en módulos CommonJS o AMD, Angular lanzará un warning en consola.
+     * En este caso podemos decirle que permita el uso de este tipo de librería
+     * mediante el archivo de configuración de angular.json
+     *
+     * architects -> build -> options -> allowedCommonJsDependencies: ["nombre-de-librería-conflicto.js"]
+     */
+    ChartsModule,   // Módulo para trabajar con gráficas (Paquete ng2-charts)
   ]
 })
 export class GraficasModule { }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import {  Label } from 'ng2-charts';
+
+/**
+ * La versión 3.x de esta librería al 21-12-2021
+ * no es compatible con Angular 12, tiene problemas de tipado estricto
+ */
 
 @Component({
   selector: 'app-barras',
@@ -11,6 +18,31 @@ export class BarrasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ];
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40 ];
   }
 
 }
