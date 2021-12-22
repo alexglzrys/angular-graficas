66d3efbf6878888c5b358b00f2733374ab247d26
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-21 18:44:01 -0600

    servicio http

--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get(`http://localhost:3000/grafica`);
+  }
 }
